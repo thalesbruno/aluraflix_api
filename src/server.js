a1eee57a9068ffe8e4c81af66ab042aa21aa7a66
@thalesbruno <thalesbrno@gmail.com>
2021-08-19 21:25:25 -0300

    Delete config dir

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require("./config");
 const videoRouter = require("./routes/videoRoutes");
 const categoriaRouter = require("./routes/categoriaRoutes");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-const db_name = config.database.host;
-const db_pass = config.database.password;
-const db_user = config.database.user;
+const db_name = process.env.DB_NAME;
+const db_pass = process.env.DB_PASSWORD;
+const db_user = process.env.DB_USER;
 
 const db_uri = `mongodb+srv://${db_user}:${db_pass}@cluster0.gzwdw.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
@@ -24,6 +22,4 @@ mongoose.connect(db_uri, {
 app.use(videoRouter);
 app.use(categoriaRouter);
 
-app.listen(port, () =>
-  console.log(`Server is running and listening at http://localhost:${port}`)
-);
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running...`));
